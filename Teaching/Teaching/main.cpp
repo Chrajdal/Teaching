1c7fb75f8cdefa0f5bd6cea7fa6b759080a4ecb4
Jan Chráska <jan.chraska@gmail.com>
2018-08-29 23:39:40 +0200

    init comit ------------------------ jednoducha kalkulacka

--- Teaching/Teaching/main.cpp
+++ Teaching/Teaching/main.cpp
@@ -1,6 +1,61 @@
 #include <iostream>
 
+/*
+Toto je viceradkovy komentar
+*/
+
+
+
+// Toto je jednoradkovy komentar
+
+// + - * / 
+
+// ==
+// !=
+// <
+// >
+// <=
+// >=
+
 int main(void)
 {
+	int x = 0;
+	int y = 0;
+
+	char answer;
+	std::cout << "Zadejte operaci: (+ - * /)\n";
+	std::cin >> answer;
+
+	if (answer == '+')
+	{
+		std::cout << "Zadejte dve cisla:\n";
+		std::cin >> x >> y;
+		std::cout << "Vysledek = " << x + y << '\n';
+	}
+	else if (answer == '-')
+	{
+		std::cout << "Zadejte dve cisla:\n";
+		std::cin >> x >> y;
+		std::cout << "Vysledek = " << x - y << '\n';
+	}
+	else if (answer == '*')
+	{
+		std::cout << "Zadejte dve cisla:\n";
+		std::cin >> x >> y;
+		std::cout << "Vysledek = " << x * y << '\n';
+	}
+	else if (answer == '/')
+	{
+		std::cout << "Zadejte dve cisla:\n";
+		std::cin >> x >> y;
+		std::cout << "Vysledek = " << x / y << '\n';
+	}
+	else
+	{
+		std::cout << "ERROR" << '\n';
+	}
+
+	
+	system("PAUSE"); // windows only
 	return 0;
 }
